5f9976fc8ec3152e70922b629b87770cbe0a7679
HungerHa <110848676+HungerHa@users.noreply.github.com>
2024-02-25 23:37:16 +0100

    - use the firmware check during power off - changed to /tmp directory for lgpio workaround due to permission issues with TempDirectory function

--- source/resources/lib/poweroff.py
+++ source/resources/lib/poweroff.py
@@ -6,6 +6,9 @@ from argonregister import *
 # Initialize I2C Bus
 bus = argonregister_initializebusobj()
 
+# check if it's the new firmware with control registers support
+argonregsupport = argonregister_checksupport(bus)
+
 # stop the fan and power off
-argonregister_setfanspeed(bus, 0)
-argonregister_signalpoweroff(bus)
+argonregister_setfanspeed(bus, 0, argonregsupport)
+argonregister_signalpoweroff(bus, argonregsupport)
